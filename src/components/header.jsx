cb313f07342416f423820ae3b2ab4306d2fb5786
Ayush <asharma7588@gmail.com>
2024-06-24 17:46:29 +0530

    herosection

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,39 +1,38 @@
 import React, { useEffect } from "react";
 const Header = () => {
-  useEffect(() => {
-    const checkActivePage = () => {
-      if (window.location.hash === "") {
-        return;
-      }
-      const page = window.location.hash;
-      document.querySelectorAll(".navbar a").forEach((activePage) => {
-        if (activePage.href.includes(page)) {
-          activePage.classList.add("active");
-        } else {
-          activePage.classList.remove("active");
-        }
-      });
-    };
-
-    window.addEventListener("hashchange", checkActivePage);
-
-    checkActivePage();
-    return () => {
-      window.removeEventListener("hashchange", checkActivePage);
-    };
-  }, []);
   return (
-    <div className="h-14 flex justify-around items-center navbar ">
-      <div className="w-1/4">
-        {/* <img  src=''></img> */}
-        Ayush Sharma
+    <div className=" flex justify-between items-center navbar w-[80%] mt-3 py-2 fixed top-0 left-0 right-0 mx-auto">
+      <div className="w-3/4 flex items-center justify-around">
+        <div className=" text-3xl font-bold text-white">
+          {/* <img  src=''></img> */}
+          Ayush <span className="bluegradient">Sharma</span>
+        </div>
+        <div className="w-[60%] flex justify-around font-bold">
+          <a href="#home">Home</a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact me</a>
+        </div>
       </div>
-      <div className="w-1/2 flex justify-around font-bold">
-        <a href="#home">Home</a>
-        <a href="#about">About me</a>
-        <a href="#projects">Projects</a>
-        <a href="#skills">Skills</a>
-        <a href="#contact">Contact me</a>
+      <div className="mr-5">
+        <button class="download-btn pixel-corners">
+          <div class="button-content">
+            <div class="svg-container">
+              <svg
+                class="download-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479 6.908l-4-4h3v-4h2v4h3l-4 4z"></path>
+              </svg>
+            </div>
+            <div class="text-container ">
+              <div class="text-white font-bold">View Resume</div>
+            </div>
+          </div>
+        </button>
       </div>
     </div>
   );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,14 +4,36 @@ import { OrbitControls, PositionPoint } from "@react-three/drei";
 import { Avatar } from "./avatar";
 const Home = () => {
   return (
-    <div className="h-screen" id="home">
-      <Canvas shadows>
-        <group position-y={-1} rotation={[Math.PI / 9, 0, 0]}>
-          <Avatar scale={[2, 2, 2]} />
-        </group>
-        {/* <OrbitControls></OrbitControls> */}
-        <ambientLight intensity={1.5}></ambientLight>
-      </Canvas>
+    <div className="h-screen w-[76%] flex m-auto mt-[7rem]" id="home">
+      {/* herosectiontext */}
+      <div className="flex flex-col gap-y-6 mt-20 ">
+        <p className="text-xl  text-white font-medium tracking-wide">
+          Hi, I'm Ayush Sharma
+        </p>
+        <p className="text-5xl text-white font-bold tracking-normal">
+          <span className="bluegradient"> Full-Stack </span>
+          Website Developer.
+        </p>
+
+        <p className="text-lg  graycolor w-2/3">
+          I am passionate Full-Stack Developer. With a strong foundation in both
+          front-end and back-end technologies.
+        </p>
+        <div>
+          <button className="getintouch p-2 mt-5">
+            <span>Get in Touch</span>
+          </button>
+        </div>
+      </div>
+      <div className="h-full">
+        <Canvas shadows>
+          <group position-y={-1} rotation={[Math.PI / 9, 0, 0]}>
+            <Avatar scale={[2.1, 2, 2]} />
+          </group>
+          <OrbitControls></OrbitControls>
+          <ambientLight intensity={2}></ambientLight>
+        </Canvas>
+      </div>
     </div>
   );
 };
